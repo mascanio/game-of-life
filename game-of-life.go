d9991b6a0eb10d0fca1206f581bff3067c156a73
Miguel Ascanio Gómez <mascanio13@gmail.com>
2024-01-21 09:48:34 +0100

    Initial OpenGL draw

---
+++ game-of-life.go
@@ -0,0 +1,214 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"runtime"
+	"strings"
+	"time"
+
+	"github.com/go-gl/gl/v4.6-core/gl"
+	"github.com/go-gl/glfw/v3.3/glfw"
+)
+
+const (
+	xrows              = 32
+	yrows              = 32
+	width              = 720
+	height             = 720
+	vertexShaderSource = `
+    #version 420
+    in vec3 vp;
+    void main() {
+        gl_Position = vec4(vp, 1.0);
+    }
+` + "\x00"
+
+	fragmentShaderSource = `
+    #version 420
+    out vec4 frag_colour;
+    void main() {
+        frag_colour = vec4(1, 1, 1, 1);
+    }
+` + "\x00"
+)
+
+type points []float32
+
+type square struct {
+	x, y float32
+}
+
+type drawBoard struct {
+	cells [][]points
+	vaos  [][]uint32
+}
+
+func makeVAO(points points) uint32 {
+	var vao uint32
+	gl.GenVertexArrays(1, &vao)
+	gl.BindVertexArray(vao)
+
+	// Create a vertex buffer object
+	var vbo uint32
+	gl.GenBuffers(1, &vbo)
+	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
+	// Copy our points into the currently bound vertex buffer.
+	gl.BufferData(gl.ARRAY_BUFFER, 4*len(points), gl.Ptr(points), gl.STATIC_DRAW)
+
+	// Specify the layout of the vertex data
+	gl.EnableVertexAttribArray(0)
+	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
+	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 0, nil)
+	return vao
+}
+
+func makeDrawBoard(xrows, yrows, xsize, ysize int) drawBoard {
+	if xsize != ysize {
+		panic("xsize and ysize must be equal")
+	}
+	squareSize := 2 / float32(xrows)
+	r := drawBoard{
+		cells: make([][]points, xrows),
+		vaos:  make([][]uint32, xrows),
+	}
+	for i := range r.cells {
+		r.cells[i] = make([]points, yrows)
+		r.vaos[i] = make([]uint32, yrows)
+		xpos := i - xrows/2
+		xnorm := float32(xpos) / float32(xrows/2)
+		for j := range r.cells[i] {
+			ypos := j - yrows/2
+			ynorm := float32(ypos) / float32(yrows/2)
+			r.cells[i][j] = squareGetPoints(square{
+				x: xnorm,
+				y: ynorm,
+			}, float32(squareSize))
+			r.vaos[i][j] = makeVAO(r.cells[i][j])
+		}
+	}
+	return r
+}
+
+func squareGetPoints(s square, size float32) points {
+	return points{
+		s.x, s.y, 0,
+		s.x + size, s.y, 0,
+		s.x + size, s.y + size, 0,
+
+		s.x, s.y, 0,
+		s.x, s.y + size, 0,
+		s.x + size, s.y + size, 0,
+	}
+}
+
+func drawCell(x, y int, drawBoard drawBoard) {
+	gl.BindVertexArray(drawBoard.vaos[x][y])
+	gl.DrawArrays(gl.TRIANGLES, 0, 6)
+}
+
+func main() {
+	runtime.LockOSThread()
+
+	window := initGlfw()
+	defer glfw.Terminate()
+
+	program := initOpenGL()
+
+	counti := 0
+	countj := 0
+	drawBoard := makeDrawBoard(xrows, yrows, width, height)
+	for !window.ShouldClose() {
+		fps := 60
+		fpsTime := glfw.GetTime()
+		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+		gl.UseProgram(program)
+
+		for i := 0; i < xrows; i++ {
+			if i == counti {
+				for j := 0; j < yrows; j++ {
+					if j == countj {
+						drawCell(i, j, drawBoard)
+					}
+				}
+			}
+		}
+		countj++
+		if countj == yrows {
+			countj = 0
+			counti = (counti + 1) % xrows
+		}
+		glfw.PollEvents()
+		window.SwapBuffers()
+		fpsTime = glfw.GetTime() - fpsTime
+		log.Println("FPS:", 1/fpsTime)
+		time.Sleep(time.Second / time.Duration(fps))
+	}
+}
+
+// initGlfw initializes glfw and returns a Window to use.
+func initGlfw() *glfw.Window {
+	if err := glfw.Init(); err != nil {
+		panic(err)
+	}
+
+	glfw.WindowHint(glfw.Resizable, glfw.False)
+	glfw.WindowHint(glfw.ContextVersionMajor, 4) // OR 2
+	glfw.WindowHint(glfw.ContextVersionMinor, 1)
+	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
+	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
+
+	window, err := glfw.CreateWindow(width, height, "Conway's Game of Life", nil, nil)
+	if err != nil {
+		panic(err)
+	}
+	window.MakeContextCurrent()
+
+	return window
+}
+
+func compileShader(source string, shaderType uint32) (uint32, error) {
+	shader := gl.CreateShader(shaderType)
+
+	csources, free := gl.Strs(source)
+	gl.ShaderSource(shader, 1, csources, nil)
+	free()
+	gl.CompileShader(shader)
+
+	var status int32
+	gl.GetShaderiv(shader, gl.COMPILE_STATUS, &status)
+	if status == gl.FALSE {
+		var logLength int32
+		gl.GetShaderiv(shader, gl.INFO_LOG_LENGTH, &logLength)
+
+		log := strings.Repeat("\x00", int(logLength+1))
+		gl.GetShaderInfoLog(shader, logLength, nil, gl.Str(log))
+
+		return 0, fmt.Errorf("failed to compile %v: %v", source, log)
+	}
+
+	return shader, nil
+}
+
+// initOpenGL initializes OpenGL and returns an intiialized program.
+func initOpenGL() uint32 {
+	if err := gl.Init(); err != nil {
+		panic(err)
+	}
+	version := gl.GoStr(gl.GetString(gl.VERSION))
+	log.Println("OpenGL version", version)
+	vertexShader, err := compileShader(vertexShaderSource, gl.VERTEX_SHADER)
+	if err != nil {
+		panic(err)
+	}
+	fragmentShader, err := compileShader(fragmentShaderSource, gl.FRAGMENT_SHADER)
+	if err != nil {
+		panic(err)
+	}
+
+	prog := gl.CreateProgram()
+	gl.AttachShader(prog, vertexShader)
+	gl.AttachShader(prog, fragmentShader)
+	gl.LinkProgram(prog)
+	return prog
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/mascanio/game-of-life
 
 go 1.21.6
+
+require (
+	github.com/go-gl/gl v0.0.0-20231021071112-07e5d0ea2e71 // indirect
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20240118000515-a250818d05e3 // indirect
+)
